5ca530f7123ff3f336e1783baba4e4cb3a8886b2
singhkatelyn <57650834+singhkatelyn@users.noreply.github.com>
2020-03-20 09:31:35 -0600

    Add files via upload

--- FriendsNetwork/friendsPage.js
+++ FriendsNetwork/friendsPage.js
@@ -18,3 +18,35 @@ function deleteCard(cardID){
   //var x = document.getElementById(cardID);
   cardID.remove();
 }
+
+//Based off of Vanessa's remove recipe 
+var numIngredients = 0;
+
+// function to add another text box for ingredient items
+function createNewElement() {
+  numIngredients++;
+
+    // First create a DIV element.
+	var txtNewInputBox = document.createElement('div');
+
+    // Then add the content (a new input box) of the element. '" + player_id + "'
+    txtNewInputBox.classList.add("input-group")
+    txtNewInputBox.innerHTML = "<input id='Ingredient " +numIngredients+ "' class='form-control py-2 rounded-pill mr-1 pr-5' type='ingredient_items' id='newIngredient'>";
+
+    // Finally put it where it is supposed to appear.
+	document.getElementById("SearchIngredients").appendChild(txtNewInputBox);
+
+  if (numIngredients == 1) {
+    document.getElementById('RemoveButton').disabled = false;
+  }
+}
+
+function removeIngredient() {
+  var el = document.getElementById('Ingredient '+numIngredients+'');
+  el.remove(); // Removes the div with the 'div-02' id
+  numIngredients--;
+
+  if (numIngredients == 0) {
+    document.getElementById('RemoveButton').disabled = true;
+  }
+}
