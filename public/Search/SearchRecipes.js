de471aee9d2bd124a7877dd9523c7863bb867c48
vanessarreyes <vanessarreyes12@gmail.com>
2020-04-07 21:25:41 -0600

    Results page can access database

--- public/Search/SearchRecipes.js
+++ public/Search/SearchRecipes.js
@@ -43,6 +43,8 @@ function getSearch() {
 
   var search = document.getElementById("search").value;
   localStorage.setItem("Search", search);
+
+
   // redirct the page to displaSearch page
   window.location.href='displaySearch.html';
 }
@@ -76,8 +78,6 @@ function getAdvancedSearch() {
     localStorage.setItem("Vegan", "Vegan");
   }
 
-
-
   var ingre = [];
   var i = 0;
   var id = "Ingredient" + (i+1);
@@ -95,4 +95,202 @@ function getAdvancedSearch() {
 
   // redirct the page to displaSearch page
   window.location.href='displaySearch.html';
+}
+
+// function that uploads search info onto displaySearch page when loaded
+function uploadSearch() {
+  var node = document.createElement('LI');
+  var textnode;
+
+  var word = localStorage.getItem('Search');
+  word = word.charAt(0).toUpperCase()+word.slice(1);
+  textnode = document.createTextNode(word);
+  node.appendChild(textnode);
+  document.getElementById("search-list").appendChild(node);
+  //document.getElementById("search").innerHTML += localStorage.getItem('Search');
+}
+
+// function to upload advanced search info onto displaSearch page when loaded
+function uploadAdvancedSearch() {
+  var node = document.createElement('LI');
+  var textnode;
+
+  textnode = document.createTextNode(localStorage.getItem('Keyword'));
+  node.appendChild(textnode);
+  document.getElementById("search-list").appendChild(node);
+  if (localStorage.getItem('Rating')) {
+    var node = document.createElement('LI');
+    textnode = document.createTextNode("Rating: " + localStorage.getItem('Rating'));
+    node.appendChild(textnode);
+    document.getElementById("search-list").appendChild(node);
+  }
+  if (localStorage.getItem('GlutenFree')) {
+    var node = document.createElement('LI');
+    textnode = document.createTextNode(localStorage.getItem('GlutenFree'));
+    node.appendChild(textnode);
+    document.getElementById("search-list").appendChild(node);
+  }
+  if (localStorage.getItem('Vegetarian')) {
+    var node = document.createElement('LI');
+    textnode = document.createTextNode(localStorage.getItem('Vegetarian'));
+    node.appendChild(textnode);
+    document.getElementById("search-list").appendChild(node);
+  }
+  if (localStorage.getItem('Vegan')) {
+    var node = document.createElement('LI');
+    textnode = document.createTextNode(localStorage.getItem('Vegan'));
+    node.appendChild(textnode);
+    document.getElementById("search-list").appendChild(node);
+  }
+
+  if (localStorage.getItem("Ingredient")) {
+    ingred = JSON.parse(localStorage.getItem("Ingredient"));
+
+    var node = document.createElement('LI');
+    for (var i = 0; i < ingred.length; i++) {
+      var text = ingred[i].charAt(0).toUpperCase()+ingred[i].slice(1);
+      if (i == 0) {
+        textnode = "Ingredient(s): " + text;
+      }
+      else {
+          textnode += ", " + text;
+      }
+    }
+    console.log(textnode)
+    textnode = document.createTextNode(textnode)
+    node.appendChild(textnode);
+    document.getElementById("search-list").appendChild(node);
+  }
+
+  if (localStorage.getItem('MaxTime')) {
+    var node = document.createElement('LI');
+    textnode = document.createTextNode("Max Time: " + localStorage.getItem('MaxTime'));
+    node.appendChild(textnode);
+    document.getElementById("search-list").appendChild(node);
+  }
+}
+
+
+// function to look in the database to find search results based on user input
+// pass in search type to know which search has been done
+function findResults(type) {
+  var ref = database.ref('recipes');
+
+  // if that checks if the search was regular or advanced
+  if (type == "regular") {
+    // getting data
+    // will either call gotData or errData function
+    ref.on('value', gotData, errData);
+  }
+}
+
+// convert minutes to hours as string
+function MinutesToHours(minutes) {
+  var conversion;
+  if (minutes > 60) {
+    var hours = Math.floor(minutes/60);
+    var minutes = (minutes%60)
+    conversion = hours.toString() + " hour(s) "+ minutes.toString() + " minute(s)";
+  }
+  else {
+    conversion = minutes.toString() + " minute(s)";
+  }
+  return conversion;
+}
+
+// function that will create recipe as a card to disply on display search
+function createCard(array) {
+  var card = "", count = 1;
+  //console.log(array[0].descrip);
+
+  var div = document.createElement("div");
+
+  for (var i = 0; i < array.length; i++) {
+    if (i == 0) {
+      card += '<div class="row">';
+      //card += '<div style="width: 30%">';
+    }
+    console.log("second");
+    card += '<div class="col-sm-6">';
+    card += '<div class="card" style="height: 17rem;">';
+    card += '<div class="card-body">';
+    card += '<h5 class="card-title">'+array[i].recipe_name+'</h5>';
+    card += '<p class="card-text">'+array[i].descrip+'</p>';
+    card += '<p class="card-text">Rating: '+array[i].rating+'</p>';
+    var time = MinutesToHours(array[i].make_time);
+    card += '<p class="card-text">Make Time: '+time+'</p>';
+    card += '</div>';
+    card += '</div>';
+    card += '</div>';
+
+    if (count%2 == 0) {
+      // end the div of card rows
+      card += '</div><div class=row style="margin-top:10px">';
+      count = 0;
+    }
+    count++;
+  }
+
+  // insert card into innerHTML of div
+  document.getElementById("results").innerHTML += card;
+
+}
+
+// function to get the data from the database
+function gotData(data) {
+  //console.log(data.val());
+
+  // set recipes to the data in database
+  var recipes = data.val();
+  // get data in array for all keys in recipes
+  var keys = Object.keys(recipes);
+  console.log(keys);
+  // array to hold found recipes
+  var recipe_array = [];
+
+  // for loop that runs through keys in recipes
+  for (var i = 0; i < keys.length; i++) {
+    // declare variables
+    var k = keys[i];
+    var recipe_name = recipes[k].recipe_name;
+    // make strings to lower case to make searching strings easier
+    var recipe_name_lower = recipe_name.toLowerCase();
+    var search = localStorage.getItem("Search").toLowerCase();
+    // if that checks if the user search is more than one word
+    if (search.split(' ').length >= 2) {
+      var split = search.split(' ');
+      var count = 0;
+      // for loop that runs through all search words to find a match in recipes
+      for (var j = 0; j < split.length; j++) {
+        var word = split[j].toString();
+        // if that checks if user search is in the recipe name
+        if (recipe_name_lower.includes(word)) {
+          count++;
+        }
+      }
+      //if that checks if there was 2 words to match recipe to display
+      if (count >= 2) {
+        // add recipe to array
+        recipe_array.push(recipes[k]);
+      }
+    }
+    // else search is only one word
+    else {
+      // if that checks if user search is in the recipe name
+      if (recipe_name_lower.includes(search)) {
+        num++;
+        // add recipe to array
+        recipe_array.push(recipes[k]);
+      }
+    }
+  }
+
+  // call function to create a card with recipe found
+  createCard(recipe_array);
+
+}
+
+function errData() {
+  console.log("Error!");
+  console.log(err);
 }
--- public/index.js
+++ public/index.js
@@ -14,36 +14,79 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 console.log(firebase);
-
-
 var database = firebase.database();
+
 var ref = database.ref('recipes');
 
-// start working with recipes to get database started
-var recipe = {
-  recipe_name: "Peanut Butter Cookies",
-  descrip: "Yummy peanut butter cookies!",
-  directions: ["Cream butter, peanut butter, and sugars together in a bowl; beat in eggs.", "In a separate bowl, sift flour, baking powder, baking soda, and salt; stir into butter mixture. Put dough in refrigerator for 1 hour.", "Roll dough into 1 inch balls and put on baking sheets. Flatten each ball with a fork, making a crisscross pattern. Bake in a preheated 375 degrees F oven for about 10 minutes or until cookies begin to brown."],
-  rating: 5,
-  gluten_free: 0,
-  vegetarian: 0,
-  vegan: 0,
-  ingredients: ["1 cup unsalted butter","1 cup crunchy peanut butter", "1 cup white sugar", "1 cup packed brown sugar", "2 eggs", "2 ½ cups all-purpose flour", "1 teaspoon baking powder", "½ teaspoon salt", "½ teaspoons baking soda"],
-  make_time: 85,
-  servings: 24
-}
-ref.push(recipe);
+//start working with recipes to get database started
 
-recipe = {
-  recipe_name: "Peanut Butter Cookies",
-  descrip: "Yummy peanut butter cookies!",
-  directions: ["Season chicken with salt, pepper and garlic powder; set aside.", "Heat a pan or skillet over medium high heat; sear chicken thigh fillets or breast fillets on both sides until golden and cooked through.", "FOR BONE IN THIGHS:", "Reduce heat after searing on both sides, cover skillet with a lid and continue cooking until the chicken is cooked through, while turning every 5 minutes until done. Alternatively, see notes for oven method.","Drain most of the excess oil from the pan,leaving about 2 tablespoons of pan juiced for added flavour.", "FOR SAUCE:", "When chicken is done and cooked through, arrange chicken skin-side up in the pan (if cooking with skin); add the garlic between the chicken and fry until fragrant (about 30 seconds). Add the honey, water, vinegar and soy sauce. Increase heat to medium-high and continue to cook until the sauce reduces down and thicken slightly (about 3-4 minutes).", "Garnish with parsley and serve over vegetables, rice, pasta or with a salad."],
-  rating: 4,
-  gluten_free: 0,
-  vegetarian: 0,
-  vegan: 0,
-  ingredients: ["6 chicken thighs, bone in or out, with or without skin", "Salt and pepper, to season", "2 teaspoons garlic powder, to season", "6 cloves garlic, crushed", "1/3 cup honey", "1/4 cup water (or chicken broth)", "2 tablespoons rice wine vinegar (or apple cider vinegar, or any white vinegar)", "1 tablespoon soy sauce"],
-  make_time: 30,
-  servings: 6
-}
-ref.push(recipe);
+// var recipe = {
+//   recipe_name: "Easy Sugar Cookies",
+//   descrip: "Quick and easy sugar cookies! They are really good, plain or with candies in them. My friend uses chocolate mints on top, and they're great!",
+//   directions: ["Step 1", "Preheat oven to 375 degrees F (190 degrees C). In a small bowl, stir together flour, baking soda, and baking powder. Set aside.", "Step 2", "In a large bowl, cream together the butter and sugar until smooth. Beat in egg and vanilla. Gradually blend in the dry ingredients. Roll rounded teaspoonfuls of dough into balls, and place onto ungreased cookie sheets.", "Step 3", "Bake 8 to 10 minutes in the preheated oven, or until golden. Let stand on cookie sheet two minutes before removing to cool on wire racks."],
+//   rating: 4,
+//   gluten_free: 0,
+//   vegetarian: 0,
+//   vegan: 0,
+//   ingredients: ["2 ¾ cups all-purpose flour", "1 teaspoon baking soda", "½ teaspoon baking powder", "1 cup butter, softened", "1 ½ cups white sugar", "1 egg", "1 teaspoon vanilla extract"],
+//   make_time: 25,
+//   servings: 48
+// }
+// ref.push(recipe);
+//
+// recipe = {
+//   recipe_name: "Bacon & Spinach Stuffed Chicken",
+//   descrip: "Hasselback your chicken by cutting shallow slits into your chicken breasts (don't cut all the way through!) and stuff each slit with bacon and a creamy spinach and artichoke filling. It will make your weeknight chicken dinner sing!",
+//   directions: ["Preheat oven to 400°. Line a large baking sheet with foil. Make slits widthwise in chicken, being careful not to cut all the way through chicken. Season with salt and pepper. Place on prepared baking sheet.", "In a medium bowl, combine cream cheese, spinach, artichokes, and ½ cup of mozzarella. Season with salt, pepper, and a pinch of red pepper flakes. Fill every other slit with cream cheese mixture and fill remaining slits with a piece of bacon. Sprinkle remaining ½ cup mozzarella on top and drizzle with oil.", "Bake until chicken is cooked through and bacon is crispy, 35 minutes."],
+//   rating: 5,
+//   gluten_free: 0,
+//   vegetarian: 0,
+//   vegan: 0,
+//   ingredients: ["4 boneless skinless chicken breasts", "Kosher salt", "Freshly ground black pepper", "4 oz. cream cheese, softened", "1/2 c. frozen spinach, defrosted and drained", "1/3 c. chopped artichoke hearts", "1 c. shredded mozzarella, divided", "Pinch crushed red pepper flakes", "4 strips bacon, cut into 4 strips", "2 tbsp. extra-virgin olive oil "],
+//   make_time: 50,
+//   servings: 4
+// }
+// ref.push(recipe);
+//
+// recipe = {
+//   recipe_name: "Vegan Chocolate Chip Cookies",
+//   descrip: "These vegan chocolate chip cookies are a one-bowl recipe that makes for a chewy gluten free cookie topped with flaky sea salt. In a word: Incredible!",
+//   directions: ["Preheat the oven to 350F and line a baking sheet with parchment paper.", "In the bottom of a large bowl, whisk together the ground flaxseed and water and let thicken for 5 minutes.", "To the same bowl, add the coconut oil, sugar, almond butter, and vanilla. Whisk until well combined. Add the almond flour and sprinkle the baking soda and salt evenly over the mixture. Use a spatula or wooden spoon to stir until well combined, adding 1 to 2 tablespoons water if the mixture is too dry. Fold in the chocolate chips.", "Use a 2-tablespoon cookie scoop to scoop the dough onto the baking sheet. Press each ball down slightly and sprinkle with flaky sea salt, if using. Bake for 10 to 13 minutes or until the edges are just starting the brown.", "Cool on the pan for 5 minutes and then transfer to a wire rack to finish cooling.", "When the cookies are completely cool, they can be stored in an airtight container or frozen. (To reheat frozen cookies, bake in a 350F oven for 5 minutes or until warmed through.)"],
+//   rating: 4,
+//   gluten_free: 0,
+//   vegetarian: 1,
+//   vegan: 1,
+//   ingredients: ["1 tablespoon ground flaxseed", "3 tablespoons water, plus more if necessary", "2 tablespoons coconut oil, softened but not melted", "1/2 cup brown sugar (or coconut sugar)", "1/4 cup almond butter", "1 teaspoon vanilla extract", "2 cups almond flour", "1/2 teaspoon baking soda", "1/4 teaspoon sea salt", "1/2 cup vegan semisweet chocolate chips", "Flaky sea salt, for sprinkling"],
+//   make_time: 25,
+//   servings: 16
+// }
+// ref.push(recipe);
+//
+// recipe = {
+//   recipe_name: "Oatmeal Chocolate Chip Cookies",
+//   descrip: "Here’s the very best oatmeal chocolate chip cookies recipe you’ll find! Chewy and packed with chocolate, a hint of molasses makes them utterly irresistible.",
+//   directions: ["In a medium bowl, whisk all-purpose flour, baking soda, baking powder, cinnamon, and kosher salt.", "In the bowl of a stand mixer, mix the butter, brown sugar, and granulated sugar on medium high for 1 to 2 minutes, scraping the bowl as necessary, until well combined.", "Add in eggs, vanilla, and molasses and combine on medium for about 30 seconds until fully combined. Add flour mixture and mix on low until combined. Add oats and 3/4 cup chocolate chips and mix on low for a few seconds until combined. Take the bowl off of the mixer and use a spatula to stir just until the oats and chocolate chips are evenly distributed.", "Transfer to a covered container. Chill 12 minutes.", "Preheat the oven to 350 degrees with two racks spaced out.", "Line two baking sheets with parchment paper. Remove the bowl with the dough from the refrigerator. Scoop cookies into 2-tablespoon drops and roll quickly into balls (try to make the scoops as even as possible to ensure uniform cookies). Place balls on baking sheets with several inches between each cookie. Flatten the tops of each ball lightly with your hand and lightly press in the additional 1/4 cup chocolate chips to the tops where needed. Sprinkle with sea salt.", "Bake for 5 minutes, then remove and rotate sheets and put them back in on the opposite racks. Bake about 5 more minutes until they just set (we like pulling them out just slightly underbaked, which makes them chewy when cooled). Cool several minutes and then transfer to wire rack to fully cool. Store at room temperature in a cookie tin for up to 3 days, or freeze for up to 3 months."],
+//   rating: 5,
+//   gluten_free: 0,
+//   vegetarian: 0,
+//   vegan: 0,
+//   ingredients: ["1 1/4 cup all-purpose flour", "1/2 teaspoon baking soda", "1/2 teaspoon baking powder", "1 teaspoon ground cinnamon", "1/2 teaspoon kosher salt", "10 tablespoons salted butter, at room temperature*", "1/3 cup brown sugar, packed", "1/3 cup granulated sugar", "2 eggs", "1 tablespoon vanilla extract", "2 tablespoons molasses", "2 cups rolled oats", "1 cup dark chocolate chips", "Flaked sea salt, for topping"],
+//   make_time: 40,
+//   servings: 24
+// }
+// ref.push(recipe);
+//
+//
+// recipe = {
+//   recipe_name: "Coconut Shrimp Curry",
+//   descrip: "This easy shrimp curry tastes better than a restaurant in under 30 minutes! It’s a Thai red curry flavored with coconut milk and curry paste.",
+//   directions: ["Start the jasmine rice (or basmati rice).", "Mince the onion. Mince the garlic. Cut the pepper into thin strips.", "In a large skillet, pot or Dutch oven, heat the oil over medium heat. Add the onion and saute for 5 minutes, until translucent. Add the garlic & red pepper and saute for 1 minute. Stir in the curry paste and saute for 1 minute.", "Add coconut milk, fish sauce, turmeric, lime zest, water, and kosher salt and bring to a simmer. Once simmering, add the shrimp and cook about 4 to 5 minutes until the shrimp is tender and opaque, depending on the size of the shrimp. Stir in 5 large leaves basil.", "To serve, spoon the shrimp and sauce over rice. Garnish with additional chopped basil and lime wedges."],
+//   rating: 4,
+//   gluten_free: 1,
+//   vegetarian: 0,
+//   vegan: 0,
+//   ingredients: ["Jasmine rice (or basmati rice), for serving", "1 yellow onion", "2 garlic cloves", "1 red bell pepper", "2 tablespoons olive oil or coconut oil", "3 tablespoons Thai red curry paste", "15-ounce can full fat coconut milk", "1 teaspoon fish sauce", "1/4 teaspoon ground turmeric", "Zest of 1 lime (plus lime wedges to garnish)", "1/2 cup water", "1 teaspoon kosher salt", "1 pound large shrimp, shell on or tail on (wild caught if possible)", "5 leaves fresh basil or Thai basil, plus more for garnish"],
+//   make_time: 25,
+//   servings: 4
+// }
+// ref.push(recipe);
